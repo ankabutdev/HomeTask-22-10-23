37d184ab5dbdd65ddebec83057de9c7488cf9931
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-10-25 11:08:34 +0500

    improve

--- DemoHomeTask-22-10-23/TaskThirdTen.cs
+++ DemoHomeTask-22-10-23/TaskThirdTen.cs
@@ -12,6 +12,26 @@ public class TaskThirdTen
 
         string[] colorset1 = { "Green", "Orange", "Yellow", "Pink" };
 
+        List<ItemMast> itemlist = new List<ItemMast>
+        {
+            new ItemMast { ItemId = 1, ItemDes = "Biscuit  " },
+            new ItemMast { ItemId = 2, ItemDes = "Chocolate" },
+            new ItemMast { ItemId = 3, ItemDes = "Butter   " },
+            new ItemMast { ItemId = 4, ItemDes = "Brade    " },
+            new ItemMast { ItemId = 5, ItemDes = "Honey    " }
+        };
+
+        List<Purchase> purchlist = new List<Purchase>
+        {
+            new Purchase { InvNo=100, ItemId = 3,  PurQty = 800 },
+            new Purchase { InvNo=101, ItemId = 2,  PurQty = 650 },
+            new Purchase { InvNo=102, ItemId = 3,  PurQty = 900 },
+            new Purchase { InvNo=103, ItemId = 4,  PurQty = 700 },
+            new Purchase { InvNo=104, ItemId = 3,  PurQty = 900 },
+            new Purchase { InvNo=105, ItemId = 4,  PurQty = 650 },
+            new Purchase { InvNo=106, ItemId = 1,  PurQty = 458 }
+        };
+
         // Task 21
         //listChars.ForEach(x => Console.Write(x + " "));
 
@@ -76,9 +96,51 @@ public class TaskThirdTen
 
         // Task 25
 
+        foreach (var item in itemlist)
+        {
+            Console.WriteLine(
+            "Item Id: {0}, Description: {1}",
+            item.ItemId,
+            item.ItemDes);
+        }
+        Console.WriteLine("--------------------------------");
+        foreach (var item in purchlist)
+        {
+            Console.WriteLine(
+            "Invoice No: {0}, Item Id : {1},  Quantity : {2}",
+            item.InvNo,
+            item.ItemId,
+            item.PurQty);
+        }
 
+        Console.WriteLine("-----------------------------------");
+
+        var result25 = (from x in itemlist
+                        join y in purchlist
+                        on x.ItemId equals y.ItemId
+                        select new
+                        {
+                            itId = x.ItemId,
+                            itDes = x.ItemDes,
+                            prQty = y.PurQty
+                        }).ToList();
+
+        result25.ForEach(x => Console.WriteLine("{0}\t{1}\t{2}", x.itId, x.itDes, x.prQty));
 
 
 
     }
+}
+
+public class ItemMast
+{
+    public int ItemId { get; set; }
+    public string ItemDes { get; set; }
+}
+
+public class Purchase
+{
+    public int InvNo { get; set; }
+    public int ItemId { get; set; }
+    public int PurQty { get; set; }
 }
